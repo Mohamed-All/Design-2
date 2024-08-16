9110b9ef02343eedd1a7017172b25dad1a554267
Mohamed-All <mohamed.ali76544321@gmail.com>
2024-08-16 17:00:48 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+
+                               /* This Project Done By Eng Mohamed Ali  */
+
+
+document.getElementById("toggleButton").onclick = function() {
+    var menu = document.getElementById("menu");
+    menu.classList.toggle("active");
+}
+document.getElementById('heartIcon').addEventListener('click', function() {
+    this.classList.toggle('red');
+});
+
